d01be89f961d0572cbe41cfef5a99f996503b3b5
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-04-02 23:58:29 -0400

    Using get() to return a boolean as the bit can only ever be 0 or 1

--- include/bit.hpp
+++ include/bit.hpp
@@ -1,24 +1,19 @@
-#ifndef BIT_HPP
-#define BIT_HPP
+#pragma once
 #include <cassert>
 #include <cstdint>
 
 namespace bit {
 inline void set(uint8_t &u8, uint8_t idx, bool val) {
   assert(idx < 8 && "Cannot set the bit at this index");
-  val ? u8 |= static_cast<uint8_t>(1 << idx)
-      : u8 &= ~static_cast<uint8_t>(1 << idx);
+  if (val) {
+    u8 |= static_cast<uint8_t>(1 << idx);
+  } else {
+    u8 &= ~static_cast<uint8_t>(1 << idx);
+  }
 }
 
-[[nodiscard]] inline uint8_t get(uint8_t u8, uint8_t idx) {
+[[nodiscard]] inline auto get(uint8_t u8, uint8_t idx) -> bool {
   assert(idx < 8 && "Cannot get the bit at this index");
   return (u8 >> idx) & 1;
 }
-
-[[nodiscard]] inline bool is_set(uint8_t u8, uint8_t idx) {
-  assert(idx < 8 && "Cannot check if the bit at this index is set");
-  return static_cast<bool>(get(u8, idx));
-}
 } // namespace bit
-
-#endif
